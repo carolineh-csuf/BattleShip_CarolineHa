f32f49f8506920f10c7ca2e4e221b6d52b53ac9d
carolineh-csuf <carolineh@csu.fullerton.edu>
2023-05-10 13:09:15 -0700

    Initial Commit

---
+++ BattleShip_CarolineHa/BattleShip_CarolineHaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleShip_CarolineHaApp.swift
+//  BattleShip_CarolineHa
+//
+//  Created by csuftitan on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BattleShip_CarolineHaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
